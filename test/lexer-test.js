fd1de56c0e5dd55aefc93703cafaedb922192b49
Sean Lee <freshdried@gmail.com>
2015-05-18 15:33:48 -0400

    implemented indentation option; wrote tests

--- test/lexer-test.js
+++ test/lexer-test.js
@@ -8,6 +8,7 @@ const biglist = fs.readFileSync(__dirname + "/data/biglist.td");
 
 const list_tokens = JSON.parse(fs.readFileSync(__dirname + "/data/list-tokens.json").toString());
 
+
 describe('Lexer', function() {
     it("should lex simple edge cases", function() {
         let lexer = new Lexer();
@@ -41,20 +42,42 @@ describe('Lexer', function() {
             list_tokens);
     });
     describe("(linebreaks = true)", function() {
+        let lexer = new Lexer({linebreaks: true});
         it("lexes initial edge cases", function() {
-            let lexer = new Lexer({linebreaks: true});
             assert.deepEqual(lexer.lex("\nA"),
                 [Tokens.EXPR(""), Tokens.EXPR("A")]);
             assert.deepEqual(lexer.lex("\nA\n \nB"),
                 [Tokens.EXPR(""), Tokens.EXPR("A"), Tokens.EXPR(""), Tokens.EXPR("B")]);
-            //ignores trailing newlines
+        });
+        it("should ignore trailing newlines", function() {
             assert.deepEqual(lexer.lex("\nA\n"),
                 [Tokens.EXPR(""), Tokens.EXPR("A")]);
-            //properly assigns newlines
+        });
+        it("should properly assigns newlines", function() {
             assert.deepEqual(lexer.lex("A\n B\n\n C"),
                 [Tokens.EXPR("A"), Tokens.INDENT(1), Tokens.EXPR("B"),  Tokens.EXPR(""), Tokens.EXPR("C"), Tokens.DEDENT(1)]);
             assert.deepEqual(lexer.lex("A\n B\n\nC"),
                 [Tokens.EXPR("A"), Tokens.INDENT(1), Tokens.EXPR("B"),  Tokens.DEDENT(1), Tokens.EXPR(""), Tokens.EXPR("C")]);
+        });
+    });
+    describe("(specified indent)", function() {
+        it("works with 4 spaces", function() {
+            let lexer = new Lexer({indent: "    "});
+            assert.deepEqual(lexer.lex("A\n    B\n            C\nD"),
+                [Tokens.EXPR("A"), Tokens.INDENT(1), Tokens.EXPR("B"), Tokens.INDENT(2), Tokens.EXPR("C"), Tokens.DEDENT(2), Tokens.DEDENT(1), Tokens.EXPR("D")]);
+        });
+        it("works with tab", function() {
+            let lexer = new Lexer({indent: "\t"});
+			assert.deepEqual(lexer.lex("A\n\tB\n\t\t\tC\nD"),
+                [Tokens.EXPR("A"), Tokens.INDENT(1), Tokens.EXPR("B"), Tokens.INDENT(2), Tokens.EXPR("C"), Tokens.DEDENT(2), Tokens.DEDENT(1), Tokens.EXPR("D")]);
+        });
+        it("works with tabtab", function() {
+            let lexer = new Lexer({indent: "\t\t"});
+			assert.deepEqual(lexer.lex("A\n\t\tB\n\t\t\t\t\t\tC\nD"),
+                [Tokens.EXPR("A"), Tokens.INDENT(1), Tokens.EXPR("B"), Tokens.INDENT(2), Tokens.EXPR("C"), Tokens.DEDENT(2), Tokens.DEDENT(1), Tokens.EXPR("D")]);
+        });
+        it("should fail to initialize Lexer with mixed indent", function() {
+            assert.throws(() => new Lexer({indent: "\t "}), /Invalid/);
         });
     });
 });
