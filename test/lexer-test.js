3f6d0a7b1da5a616cd1cc296858f871671ede3a9
Sean Lee <freshdried@gmail.com>
2015-05-18 00:30:57 -0400

    fixed indentation behavior

--- test/lexer-test.js
+++ test/lexer-test.js
@@ -4,42 +4,49 @@ import fs from "fs";
 
 const list1 = fs.readFileSync(__dirname + "/data/list1.td");
 const list2 = fs.readFileSync(__dirname + "/data/list2.td");
-const list3 = fs.readFileSync(__dirname + "/data/list3.td");
-const list4 = fs.readFileSync(__dirname + "/data/list4.td");
-const list5 = fs.readFileSync(__dirname + "/data/list5.td");
 
 const list1_tokens = JSON.parse(fs.readFileSync(__dirname + "/data/list1-tokens.json").toString());
 
 describe('Lexer', function() {
     let Lexer = require("../lib/lexer.js");
     describe('IteratedLexer', function() {
-        let lexer = new Lexer();
         it("lexes empty string", function() {
+            let lexer = new Lexer();
             assert.deepEqual(lexer.lex(""), []);
         });
         it("lexes hello", function() {
+            let lexer = new Lexer();
             assert.deepEqual(lexer.lex("hello"),
                 [Tokens.EXPR("hello")]);
         });
         it("lexes hello\\nworld", function() {
+            let lexer = new Lexer();
             assert.deepEqual(lexer.lex("hello\nworld"),
                 [Tokens.EXPR("hello"), Tokens.EXPR("world")]);
         });
+        it("fails to lex bad indentation", function() {
+            let lexer = new Lexer();
+            assert.throws(
+                function() { console.log(lexer.lex("hello\n  world\n !"))}
+                , /Indentation/);
+        });
+        it.skip("fails to lex mixed tabs and spaces", function() {
+            let lexer = new Lexer();
+            assert.throws(
+                function() { console.log(lexer.lex("hello\n  world\n\t!"))}
+                , /Indentation/);
+        });
+        it("ignores newlines by default", function() {
+            let lexer1 = new Lexer(),
+                lexer2 = new Lexer({lineBreaks: false}),
+                str = "hello\n world!\n";
+            assert.deepEqual(lexer1.lex(str), lexer2.lex(str));
+        });
         it("lexes list1.td", function() {
+            let lexer = new Lexer();
+            console.log(lexer.lex(list1.toString()));
             assert.deepEqual(lexer.lex(list1.toString()),
                 list1_tokens);
-        });
-        it("lexes list2.td", function() {
-            assert.deepEqual(lexer.lex(list2.toString()),
-                list1_tokens);
-        });
-        it("gives same tokens for list3.td and list4.td", function() {
-            assert.deepEqual(lexer.lex(list3.toString()),
-                lexer.lex(list4.toString()));
-        });
-        it("does not lex list5.td", function() {
-            //TODO: implement
-            console.log(lexer.lex(list5.toString()));
         });
     });
 });
